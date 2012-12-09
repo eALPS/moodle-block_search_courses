0ae1762d9a07c1317384e467eb36b759374bc551
Osamu HASEGAWA <good.old.memories.quoll@gmail.com>
2012-12-10 04:31:11 +0900

    検索オプションを修正

--- renderer.php
+++ renderer.php
@@ -6,7 +6,7 @@ class block_search_courses_renderer extends plugin_renderer_base {
 		$content = html_writer::start_tag('form', array('method'=>'get', 'action'=>$formtarget));
 		$content .= html_writer::start_tag('div');
 		$content .= html_writer::tag('label', s(get_string('pluginname', 'block_search_courses')), array('for'=>'coursesearchbox', 'class'=>'accesshide'));
-		$content .= html_writer::empty_tag('input', array('id'=>'searchcoursequery', 'type'=>'text', 'name'=>'query', 'value'=>s($searchvalue)));
+		$content .= html_writer::empty_tag('input', array('id'=>'searchcoursequery', 'type'=>'text', 'name'=>'search', 'value'=>s($searchvalue)));
 		$content .= html_writer::empty_tag('input', array('type'=>'submit', 'value'=>s(get_string('search'))));
 		$content .= html_writer::end_tag('div');
 		$content .= html_writer::end_tag('form');

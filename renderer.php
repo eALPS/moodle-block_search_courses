345f09a4ff5713388a8c717a251b6e981b05b884
Osamu HASEGAWA <good.old.memories.quoll@gmail.com>
2012-12-10 04:16:27 +0900

    form周りの修正

--- renderer.php
+++ renderer.php
@@ -5,7 +5,7 @@ class block_search_courses_renderer extends plugin_renderer_base {
 	public function search_form(moodle_url $formtarget, $searchvalue) {
 		$content = html_writer::start_tag('form', array('method'=>'get', 'action'=>$formtarget));
 		$content .= html_writer::start_tag('div');
-		$content .= html_writer::tag('label', s(get_string('searchinsettings', 'admin')), array('for'=>'adminsearchquery', 'class'=>'accesshide'));
+		$content .= html_writer::tag('label', s(get_string('pluginname', 'block_search_courses')), array('for'=>'coursesearchbox', 'class'=>'accesshide'));
 		$content .= html_writer::empty_tag('input', array('id'=>'searchcoursequery', 'type'=>'text', 'name'=>'query', 'value'=>s($searchvalue)));
 		$content .= html_writer::empty_tag('input', array('type'=>'submit', 'value'=>s(get_string('search'))));
 		$content .= html_writer::end_tag('div');

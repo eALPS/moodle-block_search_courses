ef267b2f0ebc83657404c927909c6b02ecc190db
Osamu HASEGAWA <good.old.memories.quoll@gmail.com>
2012-12-10 04:27:06 +0900

    関数名を変更

--- renderer.php
+++ renderer.php
@@ -2,7 +2,7 @@
 
 class block_search_courses_renderer extends plugin_renderer_base {
 
-	public function search_form(moodle_url $formtarget, $searchvalue) {
+	public function search_courses_form(moodle_url $formtarget, $searchvalue) {
 		$content = html_writer::start_tag('form', array('method'=>'get', 'action'=>$formtarget));
 		$content .= html_writer::start_tag('div');
 		$content .= html_writer::tag('label', s(get_string('pluginname', 'block_search_courses')), array('for'=>'coursesearchbox', 'class'=>'accesshide'));

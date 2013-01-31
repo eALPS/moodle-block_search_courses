239fbe3def59a1336b5bb8fccd51be6aaf644c74
Osamu HASEGAWA <good.old.memories.quoll@gmail.com>
2013-01-31 13:33:30 +0900

    言語パックja用のファイルを追加

---
+++ lang/ja/block_search_courses.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Strings for component 'block_search_courses', language 'en', branch 'MOODLE_23_STABLE'
+ *
+ * @package   block_search_courses
+ * @copyright 2012 Osamu HASEGAWA
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['configcontent'] = 'Content';
+$string['configtitle'] = 'Block title';
+$string['leaveblanktohide'] = 'leave blank to hide the title';
+$string['pluginname'] = 'コース検索';
